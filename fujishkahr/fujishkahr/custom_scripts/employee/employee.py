242d323dddf7a593f44a7e3da86a30a10cb94f87
avishna-murali <avishnachinnu26@gmail.com>
2026-02-05 15:23:06 +0530

    feat:set probation start and end dates

---
+++ fujishkahr/fujishkahr/custom_scripts/employee/employee.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2026, erpnextgit@fujishkaerp.in and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.utils import getdate ,today
+from frappe.utils import add_years
+
+@frappe.whitelist()
+def get_default_probation_period():
+	"""
+	Return default probation period in days from Fujishkahr Settings.
+	"""
+	period = frappe.db.get_single_value("Fujishkahr Settings", "default_probation_period")
+	return int(period)
