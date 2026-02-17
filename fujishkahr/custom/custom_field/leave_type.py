f05be073830e42e19fc108a6a9a50012eca3f666
avishna-murali <avishnachinnu26@gmail.com>
2026-02-17 11:56:50 +0530

    feat:Add company in doctypes

---
+++ fujishkahr/custom/custom_field/leave_type.py
@@ -0,0 +1,16 @@
+def get_leave_type_custom_fields():
+	"""
+	Custom fields that need to be added to the Leave Type DocType
+	"""
+	return {
+		"Leave Type": [
+			{
+				"fieldtype": "Link",
+				"label": "Company",
+				"fieldname": "company",
+				"insert_after": "max_continuous_days_allowed",
+				"options": "Company",
+				"reqd": 1,
+			},
+		]
+	}
