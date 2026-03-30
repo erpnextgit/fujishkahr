2c914decb3221f20e5a2f347b76e636537fa138e
avishna-murali <avishnachinnu26@gmail.com>
2026-03-30 13:47:14 +0530

    fix:hide advance amount field

--- fujishkahr/fujishkahr/doctype/advance_request/advance_request.js
+++ fujishkahr/fujishkahr/doctype/advance_request/advance_request.js
@@ -2,11 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Advance Request', {
-	refresh: function(frm) {
-		if (frm.doc.workflow_state === "Forward to CEO") {
-			frm.set_df_property('approved_amount', 'reqd', 1);
-		} else {
-			frm.set_df_property('approved_amount', 'reqd', 0);
-		}
+	refresh(frm) {
+		toggle_required(frm);
+	},
+	workflow_state(frm) {
+		toggle_required(frm);
 	}
 });
+
+function toggle_required(frm) {
+	if (frm.doc.workflow_state === "Forward to CEO") {
+		frm.set_df_property('approved_amount', 'reqd', 1);
+	} else {
+		frm.set_df_property('approved_amount', 'reqd', 0);
+	}
+}
