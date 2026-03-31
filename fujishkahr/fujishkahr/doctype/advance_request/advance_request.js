f69d3ef02eb23fa486f2b9a562b473ad7d52719c
avishna-murali <avishnachinnu26@gmail.com>
2026-03-31 16:04:36 +0530

    feat:add payment credit date and its validation

--- fujishkahr/fujishkahr/doctype/advance_request/advance_request.js
+++ fujishkahr/fujishkahr/doctype/advance_request/advance_request.js
@@ -12,8 +12,10 @@ frappe.ui.form.on('Advance Request', {
 
 function toggle_required(frm) {
 	if (frm.doc.workflow_state === "Forward to CEO") {
+		frm.set_df_property('payment_credit_date', 'reqd', 1);
 		frm.set_df_property('approved_amount', 'reqd', 1);
 	} else {
+		frm.set_df_property('payment_credit_date', 'reqd', 0);
 		frm.set_df_property('approved_amount', 'reqd', 0);
 	}
 }
